a4ff0a0150fd5f6cdc89e14e9dd82da321865be9
Sébastien Dumont <mailme@sebastiendumont.com>
2018-09-03 08:00:57 +0200

    Finished the ability to edit the block.

--- src/block/double-image/components/edit.js
+++ src/block/double-image/components/edit.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import classnames from 'classnames';
+//import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -17,8 +17,8 @@ import Inspector from './inspector';
 const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 const { compose } = wp.compose;
-const { RichText, MediaUpload, URLInput, mediaUpload } = wp.editor;
-const { Dashicon, IconButton, DropZone } = wp.components;
+const { RichText, MediaUpload, mediaUpload } = wp.editor;
+const { Button, Dashicon, DropZone } = wp.components;
 
 /**
  * Block edit function
@@ -32,21 +32,30 @@ export default compose( Colors ) ( class Edit extends Component {
 		this.addImageTwo      = this.addImageTwo.bind( this );
 		this.onSelectImageOne = this.onSelectImageOne.bind( this );
 		this.onSelectImageTwo = this.onSelectImageTwo.bind( this );
+		this.offFocusImage    = this.offFocusImage.bind( this );
 
 		this.state = {
-			buttonFocused: false,
+			imageFocused: false,
 		};
+	}
+
+	offFocusImage() {
+		if ( this.state.imageFocused ) {
+			this.setState( {
+				imageFocused: false,
+			} );
+		}
 	}
 
 	onSelectImageOne( media ) {
 		if ( media && media.url ) {
-			this.props.setAttributes( { firstImageURL: media.sizes.full.url, firstImageID: media.id } );
+			this.props.setAttributes( { firstImageURL: media.url, firstImageID: media.id } );
 		}
 	}
 
 	onSelectImageTwo( media ) {
 		if ( media && media.url ) {
-			this.props.setAttributes( { secondImageURL: media.sizes.full.url, secondImageID: media.id } );
+			this.props.setAttributes( { secondImageURL: media.url, secondImageID: media.id } );
 		}
 	}
 
@@ -70,17 +79,16 @@ export default compose( Colors ) ( class Edit extends Component {
 
 		const {
 			attributes,
-			setAttributes,
 			className,
 			isSelected,
 			mergeBlocks,
 			onReplace,
+			setAttributes,
 		} = this.props;
 
 		const {
 			format,
 			align,
-			textAlign,
 			showOverlays,
 			firstImageID,
 			firstImageURL,
@@ -110,6 +118,12 @@ export default compose( Colors ) ( class Edit extends Component {
 		const onUploadImageOne = ( media ) => setAttributes( { firstImageURL: media.sizes.full.url, firstImageID: media.id } );
 		const onUploadImageTwo = ( media ) => setAttributes( { secondImageURL: media.sizes.full.url, secondImageID: media.id } );
 
+		const styleFirstImage  = backgroundImageStyles( firstImageURL );
+		const styleSecondImage = backgroundImageStyles( secondImageURL );
+
+		const firstTextPosition = textPosition( firstImageTextPosition );
+		const secondTextPosition = textPosition( secondImageTextPosition );
+
 		return [
 			<Fragment>
 				{ isSelected && (
@@ -123,48 +137,90 @@ export default compose( Colors ) ( class Edit extends Component {
 					/>
 				) }
 				<DoubleImage { ...this.props }>
-					{ dropZoneOne }
-					<div class="image-block left">
+					<div 
+					className={ 'image-block left' + `${ showOverlays ? ' show-overlay' : '' }` + `${ firstImageURL ? ' has-image-set' : '' }`} 
+					style={ styleFirstImage }
+					>
+						{ dropZoneOne }
 						<MediaUpload
 							onSelect={ onUploadImageOne }
 							type="image"
 							value={ firstImageID }
 							render={ ( { open } ) => (
 								<Button onClick={ open }>
-									{ ! firstImageID ? <Dashicon icon="format-image" /> :
-										<img
-											className={ `${ className }__avatar-img` }
-											src={ firstImageURL }
-											alt="image"
-										/>
-									}
+								{ ! firstImageID ? <Dashicon icon="format-image" /> : '' }
 								</Button>
 							) }
 						>
 						</MediaUpload>
+						{ ! firstImageID ? '' :
+							<div className={ 'overlay-container ' + `${firstTextPosition}`}>
+								{ ( ( firstImageText && firstImageText.length > 0 ) || isSelected ) && (
+									<div class="overlay-text left">
+									<RichText
+										multiline="p"
+										tagName="p"
+										placeholder={ __( 'Optional overlay text...' ) }
+										value={ firstImageText }
+										className={ `${ className }__first-overlay-text` }
+										style={{ color: firstImageTextColor }}
+										onMerge={ mergeBlocks }
+										onChange={ ( value ) => setAttributes( { firstImageText: value } ) }
+										unstableOnFocus={ this.offFocusImage }
+										keepPlaceholderOnFocus
+									/>
+									</div>
+								) }
+							</div>
+						}
 					</div>
-					{ dropZoneTwo }
-					<div class="image-block right">
+					<div 
+					className={ 'image-block right' + `${ showOverlays ? ' show-overlay' : '' }` + `${ secondImageURL ? ' has-image-set' : '' }`} 
+					style={ styleSecondImage }
+					>
+						{ dropZoneTwo }
 						<MediaUpload
 							onSelect={ onUploadImageTwo }
 							type="image"
 							value={ secondImageID }
 							render={ ( { open } ) => (
 								<Button onClick={ open }>
-									{ ! secondImageID ? <Dashicon icon="format-image" /> :
-										<img
-											className={ `${ className }__avatar-img` }
-											src={ secondImageURL }
-											alt="image"
-										/>
-									}
+								{ ! secondImageID ? <Dashicon icon="format-image" /> : '' }
 								</Button>
 							) }
 						>
 						</MediaUpload>
+						{ ! secondImageID ? '' :
+							<div className={ 'overlay-container ' + `${secondTextPosition}`}>
+								{ ( ( secondImageText && secondImageText.length > 0 ) || isSelected ) && (
+									<div class="overlay-text right">
+									<RichText
+										multiline="p"
+										tagName="p"
+										placeholder={ __( 'Optional overlay text...' ) }
+										value={ secondImageText }
+										className={ `${ className }__second-overlay-text` }
+										style={{ color: secondImageTextColor }}
+										onMerge={ mergeBlocks }
+										onChange={ ( value ) => setAttributes( { secondImageText: value } ) }
+										unstableOnFocus={ this.offFocusImage }
+										keepPlaceholderOnFocus
+									/>
+									</div>
+								) }
+							</div>
+						}
 					</div>
 				</DoubleImage>
 			</Fragment>
 		];
 	}
 } );
+
+function backgroundImageStyles( url ) {
+	return url ? { backgroundImage: `url(${ url })` } : undefined;
+}
+
+function textPosition( position ) {
+	return position ? 'text-' + `${position}` : 'text-top';
+}
