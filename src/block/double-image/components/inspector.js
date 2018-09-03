71c33f2171fd2c405341eb511fbcbd45144abdb9
Sébastien Dumont <mailme@sebastiendumont.com>
2018-09-03 07:41:51 +0200

    Finished Inspector for Block Settings

--- src/block/double-image/components/inspector.js
+++ src/block/double-image/components/inspector.js
@@ -7,10 +7,10 @@ import Colors from './colors';
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
-const { Component } = wp.element;
+const { Component, Fragment } = wp.element;
 const { compose } = wp.compose;
-const { InspectorControls, ColorPalette } = wp.blocks;
-const { PanelBody, PanelColor, PanelRow, ToggleControl, SelectControl } = wp.components;
+const { InspectorControls, ColorPalette } = wp.editor;
+const { PanelBody, PanelColor, Toolbar, ToggleControl, SelectControl } = wp.components;
 
 /**
  * Inspector controls
@@ -32,64 +32,110 @@ export default compose( Colors ) ( class Inspector extends Component {
 			format,
 			showOverlays,
 			firstImageTextColor,
-			secondImageTextColor
+			firstImageTextPosition,
+			secondImageTextColor,
+			secondImageTextPosition
 		} = attributes;
 
 		return (
-			<InspectorControls key="inspector">
+			<Fragment>
+				<InspectorControls>
+					<PanelBody title={ __( 'Block Settings' ) } className="double-image__inspector-block-settings-panel-body">
+						<SelectControl
+							key={ 'format-selector' }
+							label={ __( 'Image Layout' ) }
+							value={ format ? format : '1-4' }
+							onChange={ ( value ) => setAttributes({ format: value }) }
+							options={ [
+								{
+									label: __( '1-4' ),
+									value: '1-4',
+								},
+								{
+									label: __( '4-1' ),
+									value: '4-1',
+								},
+								{
+									label: __( 'Even' ),
+									value: 'even',
+								},
+								{
+									label: __( 'Full Stack' ),
+									value: 'full-stack',
+								}
+							]}
+						/>
 
-				<SelectControl
-					key={ 'format-selector' }
-					label={ __( 'Format' ) }
-					value={ format ? format : '1-4' }
-					onChange={ ( value ) => setAttributes({ format: value }) }
-					options={ [
-						{
-							label: __( '1-4' ),
-							value: '1-4',
-						},
-						{
-							label: __( '4-1' ),
-							value: '4-1',
-						},
-						{
-							label: __( 'Even' ),
-							value: 'even',
-						},
-						{
-							label: __( 'Full Stack' ),
-							value: 'full-stack',
-						}
-					]}
-				/>
+						<SelectControl
+							key={ 'overlay-text-postition-selector' }
+							label={ __( 'First Image Overlay Text Position' ) }
+							value={ firstImageTextPosition ? firstImageTextPosition : 'text-top' }
+							onChange={ ( value ) => setAttributes({ firstImageTextPosition: value }) }
+							options={ [
+								{
+									label: __( 'Top' ),
+									value: 'top',
+								},
+								{
+									label: __( 'Bottom' ),
+									value: 'bottom',
+								},
+							] }
+							default={ 'top' }
+						/>
 
-				<ToggleControl
-					label={ __( 'Show Overlays' ) }
-					checked={ !! showOverlays }
-					onChange={ () => setAttributes( { showOverlays: showOverlays } ) }
-				/>
+						<SelectControl
+							key={ 'overlay-text-postition-selector' }
+							label={ __( 'Second Image Overlay Text Position' ) }
+							value={ secondImageTextPosition ? secondImageTextPosition : 'text-top' }
+							onChange={ ( value ) => setAttributes({ secondImageTextPosition: value }) }
+							options={ [
+								{
+									label: __( 'Top' ),
+									value: 'top',
+								},
+								{
+									label: __( 'Bottom' ),
+									value: 'bottom',
+								},
+							] }
+							default={ 'bottom' }
+						/>
 
-				<PanelBody title={ __( 'First Image Settings' ) } className="double-image__inspector-block-settings-panel-body">
-					<PanelColor title={ __( 'First Text Color' ) } colorValue={ firstImageTextColor }>
+						<ToggleControl
+							label={ __( 'Show Overlays' ) }
+							checked={ !! showOverlays }
+							onChange={ () => setAttributes( { showOverlays: ! showOverlays } ) }
+						/>
+					</PanelBody>
+
+					<PanelColor 
+						title={ __( 'First Image Overlay Text Color' ) } 
+						colorValue={ firstImageTextColor }
+						onChange={ ( colorValue ) => setAttributes( { firstImageTextColor: colorValue } ) } 
+						initialOpen={ false }
+					>
 						<ColorPalette
-							label={ __( 'First Text Color' ) }
+							label={ __( 'First Image Overlay Text Color' ) }
 							value={ firstImageTextColor }
 							onChange={ ( colorValue ) => setAttributes( { firstImageTextColor: colorValue } ) }
 						/>
 					</PanelColor>
-				</PanelBody>
 
-				<PanelBody title={ __( 'Second Image Settings' ) } className="double-image__inspector-block-settings-panel-body">
-					<PanelColor title={ __( 'Second Text Color' ) } colorValue={ secondImageTextColor }>
+					<PanelColor 
+						title={ __( 'Second Image Overlay Text Color' ) } 
+						colorValue={ secondImageTextColor } 
+						onChange={ ( colorValue ) => setAttributes( { secondImageTextColor: colorValue } ) } 
+						initialOpen={ false }
+						>
 						<ColorPalette
-							label={ __( 'Second Text Color' ) }
+							label={ __( 'Second Image Overlay Text Color' ) }
 							value={ secondImageTextColor }
 							onChange={ ( colorValue ) => setAttributes( { secondImageTextColor: colorValue } ) }
 						/>
 					</PanelColor>
-				</PanelBody>
-
-			</InspectorControls>
+				</InspectorControls>
+			</Fragment>
 		);
 	}
 } );
